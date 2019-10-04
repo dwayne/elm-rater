0f153312f708b90ecf4d666d82dab93e9fcdab81
Dwayne Crooks <me@dwaynecrooks.com>
2019-10-04 10:36:52 -0400

    Make the rater reusable

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
-import Html.Events as Events
+import Html exposing (Html)
+
+import Rater
 
 
 main : Program () Model Msg
@@ -19,61 +19,26 @@ main =
 
 
 type alias Model =
-  { rater : Rater }
-
-
-type Rater
-  = Fixed Int
-  | Transient Int Int
+  { rater : Rater.State }
 
 
 init : Model
 init =
-  { rater = Fixed 3 }
+  { rater = Rater.initial 3 }
 
 
 -- UPDATE
 
 
 type Msg
-  = MouseOver Int
-  | MouseOut
-  | Clicked Int
+  = NewRaterMsg Rater.Msg
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    MouseOver transientValue ->
-      case model.rater of
-        Fixed fixedValue ->
-          { model | rater = Transient fixedValue transientValue }
-
-        Transient fixedValue _ ->
-          { model | rater = Transient fixedValue transientValue }
-
-    MouseOut ->
-      case model.rater of
-        Fixed _ ->
-          model
-
-        Transient fixedValue _ ->
-          { model | rater = Fixed fixedValue }
-
-    Clicked newFixedValue ->
-      let
-        currentFixedValue =
-          case model.rater of
-            Fixed fixedValue ->
-              fixedValue
-
-            Transient fixedValue _ ->
-              fixedValue
-      in
-        if newFixedValue == currentFixedValue then
-          { model | rater = Fixed 0 }
-        else
-          { model | rater = Fixed newFixedValue }
+    NewRaterMsg raterMsg ->
+      { model | rater = Rater.update raterMsg model.rater }
 
 
 -- VIEW
@@ -81,48 +46,4 @@ update msg model =
 
 view : Model -> Html Msg
 view { rater } =
-  let
-    rating =
-      case rater of
-        Fixed fixedValue ->
-          fixedValue
-
-        Transient _ transientValue ->
-          transientValue
-  in
-    viewRater 5 rating
-
-
-viewRater : Int -> Int -> Html Msg
-viewRater total rating =
-  div
-    [ class "rater"
-    , Events.onMouseOut MouseOut
-    ]
-    (viewStars 1 rating selected ++ viewStars (rating + 1) total unselected)
-
-
-viewStars : Int -> Int -> Html Msg -> List (Html Msg)
-viewStars low high star =
-  List.range low high
-    |> List.map (viewStar star)
-
-
-viewStar : Html Msg -> Int -> Html Msg
-viewStar star value =
-  div
-    [ class "rater__star"
-    , Events.onMouseOver (MouseOver value)
-    , Events.onClick (Clicked value)
-    ]
-    [ star ]
-
-
-selected : Html msg
-selected =
-  text "\u{2605}"
-
-
-unselected : Html msg
-unselected =
-  text "\u{2606}"
+  Html.map NewRaterMsg (Rater.view 5 rater)
---
+++ src/Rater.elm
@@ -0,0 +1,113 @@
+module Rater exposing
+  ( State
+  , initial
+
+  , Msg
+  , update
+
+  , view
+  )
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+import Html.Events as Events
+
+
+type State
+  = Fixed Int
+  | Transient Int Int
+
+
+initial : Int -> State
+initial value =
+  Fixed value
+
+
+type Msg
+  = MouseOver Int
+  | MouseOut
+  | Clicked Int
+
+
+update : Msg -> State -> State
+update msg state =
+  case msg of
+    MouseOver transientValue ->
+      case state of
+        Fixed fixedValue ->
+          Transient fixedValue transientValue
+
+        Transient fixedValue _ ->
+          Transient fixedValue transientValue
+
+    MouseOut ->
+      case state of
+        Fixed _ ->
+          state
+
+        Transient fixedValue _ ->
+          Fixed fixedValue
+
+    Clicked newFixedValue ->
+      let
+        currentFixedValue =
+          case state of
+            Fixed fixedValue ->
+              fixedValue
+
+            Transient fixedValue _ ->
+              fixedValue
+      in
+        if newFixedValue == currentFixedValue then
+          Fixed 0
+        else
+          Fixed newFixedValue
+
+
+view : Int -> State -> Html Msg
+view total state =
+  let
+    rating =
+      case state of
+        Fixed fixedValue ->
+          fixedValue
+
+        Transient _ transientValue ->
+          transientValue
+  in
+    viewRater total rating
+
+
+viewRater : Int -> Int -> Html Msg
+viewRater total rating =
+  div
+    [ class "rater"
+    , Events.onMouseOut MouseOut
+    ]
+    (viewStars 1 rating selected ++ viewStars (rating + 1) total unselected)
+
+
+viewStars : Int -> Int -> Html Msg -> List (Html Msg)
+viewStars low high star =
+  List.range low high
+    |> List.map (viewStar star)
+
+
+viewStar : Html Msg -> Int -> Html Msg
+viewStar star value =
+  div
+    [ class "rater__star"
+    , Events.onMouseOver (MouseOver value)
+    , Events.onClick (Clicked value)
+    ]
+    [ star ]
+
+
+selected : Html msg
+selected =
+  text "\u{2605}"
+
+
+unselected : Html msg
+unselected =
+  text "\u{2606}"
