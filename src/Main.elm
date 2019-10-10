48539bd65108e7978c0b36c17e7dfc54428daec5
Dwayne Crooks <me@dwaynecrooks.com>
2019-10-10 07:48:20 -0400

    Display 5 stars

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Html exposing (Html, div, span, text)
+import Html.Attributes as A
 
 
 main : Html msg
 main =
-  text "Hello, world!"
+  viewRater
+
+
+-- VIEW
+
+
+viewRater : Html msg
+viewRater =
+  div [ A.class "rater" ]
+    [ viewStar selected
+    , viewStar selected
+    , viewStar selected
+    , viewStar unselected
+    , viewStar unselected
+    ]
+
+
+viewStar : Html msg -> Html msg
+viewStar star =
+  div [ A.class "rater__star-wrapper" ] [ star ]
+
+
+selected : Html msg
+selected =
+  span [ A.class "rater__star" ] [ text "\u{2605}" ]
+
+
+unselected : Html msg
+unselected =
+  span [ A.class "rater__star" ] [ text "\u{2606}" ]
